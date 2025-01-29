9d1304a9d1ab022d2c16a1419d6facc9e1e4e1e3
MiyuuYamasaki <y-miyu-2429@occ.co.jp>
2025-01-29 17:49:23 +0900

    change unique to first

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 export async function POST(req: Request) {
   const { username, password } = await req.json();
 
-  const user = await prisma.user.findUnique({
+  const user = await prisma.user.findFirst({
     where: {
       userId: username,
       password: password,
